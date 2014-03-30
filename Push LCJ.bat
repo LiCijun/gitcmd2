38f78366e3b83400f265c82ee4d80fc57fd72a22
li <li@aihlp.com>
2014-03-30 23:25:01 +0800

    111

---
+++ Push LCJ.bat
@@ -0,0 +1,13 @@
+@echo off
+ for /d %%s in (%~dp0..\*) do (
+echo lcj
+cd %%s
+echo %%s
+echo.
+git.exe push --all --progress  "lcj"
+git.exe push --tags --progress  "lcj" master:master
+echo.
+echo.
+echo.
+)
+pause
